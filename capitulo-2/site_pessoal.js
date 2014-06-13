ba4578a01d605b2e0f67b0b092284995d58602db
Eduardo Tanaka <edutanaka@users.noreply.github.com>
2014-06-13 18:56:48 -0300

    Update site_pessoal.js
    corrigindo o nome do método

--- capitulo-2/site_pessoal.js
+++ capitulo-2/site_pessoal.js
@@ -6,7 +6,7 @@ var server = http.createServer(function(request, response){
   // A constante __dirname retorna o diretório raíz da aplicação.
   fs.readFile(__dirname + '/index.html', function(err, html){
 
-    response.writeHeader(200, {'Content-Type': 'text/html'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(html); 
     response.end();    
   });
